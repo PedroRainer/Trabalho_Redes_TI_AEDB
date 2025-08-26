956ca97635fcd22609960ee37f40178f478d5e2b
r41n3r <pedrorainer2@gmail.com>
2025-08-26 18:06:12 -0300

    Flamengo ate morrer

--- cmd/run-local.sh
+++ cmd/run-local.sh
@@ -5,7 +5,7 @@ tmux split-window -h
 tmux split-window -v -t "redes:dev.1"
 tmux split-window -v -t "redes:dev.0"
 
-tmux send-keys -t "redes:dev.0" "cd frontend && npm run dev" C-m
+tmux send-keys -t "redes:dev.0""" "cd frontend && npm run dev" C-m
 tmux send-keys -t "redes:dev.1" "cd views && go run ./main" C-m
 
 tmux attach-session -t "redes" 
